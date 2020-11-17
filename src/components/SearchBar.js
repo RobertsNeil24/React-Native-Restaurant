4bf0928071f4e1990b5f0ffc1611637344540b95
Neil Roberts <neil.roberts@absa.africa>
2020-11-17 09:47:51 +0200

    wired up api

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
  return (
   <View style={styles.backgroundStyle}>
     <Feather name="search" style={styles.iconStyle} />
     <TextInput 
       placeholder="Search"
+      autoCapitalize='none'
+      autoCorrect={false}
       style={styles.inputStyle}
+      value={term}
+      onChangeText={newTerm => onTermChange(newTerm)}
+      onEndEditing={() => onTermSubmit()}
     />
   </View>
  )
